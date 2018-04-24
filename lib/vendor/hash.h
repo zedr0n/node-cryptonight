6c641641c9e47b8f04863e6ee691b3143936839e
Joon_ <jblee@glosfer.com>
2018-04-24 14:35:31 +0900

    delete deprecated source and fix variant to 1

--- lib/vendor/hash.h
+++ lib/vendor/hash.h
@@ -72,11 +72,11 @@ namespace crypto {
   }
 
   inline void cn_slow_hash(const void *data, std::size_t length, hash &hash, int variant = 1) {
-    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), variant, 0/*prehashed*/);
+    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), 1, 0/*prehashed*/);
   }
 
   inline void cn_slow_hash_prehashed(const void *data, std::size_t length, hash &hash, int variant = 1) {
-    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), variant, 1/*prehashed*/);
+    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), 1, 1/*prehashed*/);
   }
 
   inline void tree_hash(const hash *hashes, std::size_t count, hash &root_hash) {
